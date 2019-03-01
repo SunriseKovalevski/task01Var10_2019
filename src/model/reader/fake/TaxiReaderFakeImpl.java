5e7569d8add9f1826dcf33d1b2bd496671748681
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2019-03-01 20:26:54 +0300

    add: private buildtaxi field into TaxiReaderFakeImpl and create Calculator class into model.calculator

--- src/model/reader/fake/TaxiReaderFakeImpl.java
+++ src/model/reader/fake/TaxiReaderFakeImpl.java
@@ -11,11 +11,21 @@ public class TaxiReaderFakeImpl  implements TaxiReader {
     public List<Taxi> read() {
         List<Taxi> taxopark = new ArrayList<>();
         taxopark.add(buildTaxi(100,15,205));
+        taxopark.add(buildTaxi(130, 12, 220));
+        taxopark.add(buildTaxi(120, 11, 300));
+        taxopark.add(buildTaxi(110, 18, 290));
+        return taxopark;
     }
 
     private void build(Taxi taxi, Integer cost, Integer fuelRate, Integer speed){
         taxi.setCost(cost);
         taxi.setFuelRate(fuelRate);
         taxi.setSpeed(speed);
+    }
+
+    private Taxi buildTaxi(Integer cost, Integer fuelRate, Integer speed) {
+        Taxi taxi = new Taxi();
+        build(taxi, cost, fuelRate, speed);
+        return taxi;
     }
 }
