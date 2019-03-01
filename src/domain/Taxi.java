e983e23247fa5e52907aac3af60454e964c4d968
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2019-03-01 19:41:06 +0300

    add: some fields and they getters\setters in Taxi class

--- src/domain/Taxi.java
+++ src/domain/Taxi.java
@@ -1,4 +1,31 @@
 package domain;
 
 public class Taxi {
+    private Integer cost;
+    private Integer fuelRate;
+    private Integer speed;
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public Integer getFuelRate() {
+        return fuelRate;
+    }
+
+    public void setFuelRate(Integer fuelRate) {
+        this.fuelRate = fuelRate;
+    }
+
+    public Integer getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Integer speed) {
+        this.speed = speed;
+    }
 }
