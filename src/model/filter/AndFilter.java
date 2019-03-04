4af544697cebce7bdd3862d73d3b0a4658d40e39
SunriseKovalevski <SunriseKovalevsky283@gmail.com>
2019-03-04 20:43:31 +0300

    feat: add filter "and" AndFilter class

---
+++ src/model/filter/AndFilter.java
@@ -0,0 +1,18 @@
+package model.filter;
+
+import domain.Taxi;
+
+public class AndFilter  implements Filter{
+    private Filter a;
+    private Filter b;
+
+    public AndFilter(Filter a, Filter b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    public boolean check(Taxi taxi) {
+        return a.check(Taxi) && b.check(Taxi);
+    }
+}
